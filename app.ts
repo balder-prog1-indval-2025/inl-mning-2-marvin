55d210090335129750cb4ca3339e504503f61a87
Marvin <jagdvert@gmail.com>
2026-05-29 09:45:38 +0200

    upd

--- app.ts
+++ app.ts
@@ -4,11 +4,11 @@ let yttreGrid = g.cell
 let timeout = 0 
 let rad = 0 
 let column = 0
-let block = [
+/*let block = [
   {
     rad: 0,
     column: 0
-  },
+  },                                                                                                                            
   {
     rad:0,
     column:1 
@@ -22,9 +22,53 @@ let block = [
     column:1 
   }
 ]
+*/
+let avlång = [
+  {rad:0,
+    column:0
 
-let height = 2 
-let width = 2 
+  },
+  {rad:1,
+  column:0
+
+},
+{rad:2,
+column: 0 
+
+},
+{rad:3,
+column:0 
+
+},
+]
+
+
+let zVänster= [
+  {rad: 0,
+    column:0 
+    
+  },
+   
+  {rad: 0,
+      column: 1
+  },
+  
+  {rad: 1, 
+  column: 1   
+  },
+  
+  {rad:1, 
+  column:2
+  },
+  ]
+
+
+  let colojr = randomItem ("red", "green", "blue", "yellow", "purple", "white")
+
+
+
+/*  let h = 4
+let w = 1
 let colojr = randomItem ("red", "green", "blue", "yellow", "purple", "white")
 update =  () => {
     
@@ -36,9 +80,14 @@ update =  () => {
 
         
         timeout = 0 
-        if( rad != 18 + height && g.cell(rad+height,column).color == null) { 
-          g.cell(rad,column).color = null
-            rad++
+        if( rad != 14 + h && g.cell(rad+h,column).color == null) { 
+         
+          
+          for(let i = 0 ; i< avlång.length; i++){
+            g.cell(rad + avlång[i].rad ,column + avlång[i].column) .color = null
+          }
+        
+          rad++
 
         }
         else {rad = 0
@@ -48,10 +97,157 @@ update =  () => {
     }
   if (keyboard.d){
     g.cell(rad,column).color = null
-column++ 
+   
+    for(let i = 0 ; i< avlång.length; i++){
+      g.cell(rad + avlång[i].rad ,column + avlång[i].column) .color = null
+    }
+
+    column++ 
 
   }
   if (keyboard.a){g.cell(rad,column).color = null
+    
+    for(let i = 0 ; i< avlång.length; i++){
+      g.cell(rad + avlång[i].rad ,column + avlång[i].column) .color = null
+    }
+    
+    column--
+  }
+  if ( column == 12 ){ 
+    column = 11
+  }
+  if(column < 0) {
+  column = 0
+  } 
+  //if (g.cell(rad,column).color != "" )
+
+  for(let i = 0 ; i< avlång.length; i++){
+    g.cell(rad + avlång[i].rad ,column + avlång[i].column) .color = colojr
+  }
+
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+let längd = 2 
+let bredd = 4
+
+update =  () => {
+    
+  clear ()
+  g.draw ()
+  timeout += deltaTime
+  if (timeout > 100 ){
+      
+
+      
+      timeout = 0 
+      if( rad != 18 + längd&& g.cell(rad+längd,column).color == null) { 
+       
+        
+        for(let i = 0 ; i< zVänster.length; i++){
+          g.cell(rad + zVänster[i].rad ,column + zVänster[i].column) .color = null
+        }
+      
+        rad++
+
+      }
+      else {rad = 0
+         colojr = randomItem ("red", "green", "blue", "yellow", "purple", "white")
+     }
+
+  }
+if (keyboard.d){
+  g.cell(rad,column).color = null
+ 
+  for(let i = 0 ; i< zVänster.length; i++){
+    g.cell(rad + zVänster[i].rad ,column + zVänster[i].column) .color = null
+  }
+
+  column++ 
+
+}
+if (keyboard.a){g.cell(rad,column).color = null
+  
+  for(let i = 0 ; i< zVänster.length; i++){
+    g.cell(rad + zVänster[i].rad ,column + zVänster[i].column) .color = null
+  }
+  
+  column--
+}
+if ( column == 12 ){ 
+  column = 11
+}
+if(column < 0) {
+column = 0
+} 
+//if (g.cell(rad,column).color != "" )
+
+for(let i = 0 ; i< zVänster.length; i++){
+  g.cell(rad + zVänster[i].rad ,column + zVänster[i].column) .color = colojr
+}
+
+
+}
+
+
+
+
+
+
+/*let height = 2 
+let width = 2 
+//let colojr = randomItem ("red", "green", "blue", "yellow", "purple", "white")
+update =  () => {
+    
+    clear ()
+    g.draw ()
+    timeout += deltaTime
+    if (timeout > 100 ){
+        
+
+        
+        timeout = 0 
+        if( rad != 18 + height && g.cell(rad+height,column).color == null) { 
+         
+          
+          for(let i = 0 ; i< block.length; i++){
+            g.cell(rad + block[i].rad ,column + block[i].column) .color = null
+          }
+        
+          rad++
+
+        }
+        else {rad = 0
+           colojr = randomItem ("red", "green", "blue", "yellow", "purple", "white")
+       }
+
+    }
+  if (keyboard.d){
+    g.cell(rad,column).color = null
+   
+    for(let i = 0 ; i< block.length; i++){
+      g.cell(rad + block[i].rad ,column + block[i].column) .color = null
+    }
+
+    column++ 
+
+  }
+  if (keyboard.a){g.cell(rad,column).color = null
+    
+    for(let i = 0 ; i< block.length; i++){
+      g.cell(rad + block[i].rad ,column + block[i].column) .color = null
+    }
+    
     column--
   }
   if ( column == 12 ){ 
@@ -66,6 +262,7 @@ column++
     g.cell(rad + block[i].rad ,column + block[i].column) .color = colojr
   }
 
+
 //if( rad == 21) {
 //g.cell(21, column )
  
@@ -73,5 +270,5 @@ column++
 //}
 
 }
-
+*/
 
